a33969147ba3f4a8422a5def3055f415453cf807
siqq <144137886+siqqdev@users.noreply.github.com>
2024-05-07 05:49:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const bot = new TelegramBot(token, { polling: true })
 bot.on('message', async (msg) => {
     console.log('started')
     if (msg.text === '/start') {
-        const chatId = msg.chat.id
-        await bot.sendMessage(chatId, `Рады вас видеть, ${msg.from.first_name}! Проводится регистрация, пожалуйста подождите`)
-        try {
-            const res = await axios.post('http://62.113.99.97/api/users/register', { telegramId: msg.from.id.toString(), username: msg.from.username, nickname: msg.from.first_name })
-            console.log(res.statusText)
-            await bot.sendMessage(chatId, `Регистрация прошла успешно ${res.data.username}`)
-        } catch (error) {
-            console.error('Ошибка регистрации:', error)
-            await bot.sendMessage(chatId, `Произошла ошибка при регистрации: ${error.message}`)
-        }
+    const chatId = msg.chat.id
+    const telegramId = msg.from.id.toString()
+    const username = msg.from.username || 'не указан'
+    const nickname = msg.from.first_name || 'не указан'
+    
+    await bot.sendMessage(chatId, `Отправляю запрос на регистрацию.\nТелеграм ID: ${telegramId}\nUsername: ${username}\nNickname: ${nickname}`)
+    
+    try {
+        const res = await axios.post('http://62.113.99.97/api/users/register', { telegramId, username, nickname })
+        console.log(res.statusText)
+        await bot.sendMessage(chatId, `Регистрация прошла успешно ${res.data.username}`)
+    } catch (error) {
+        console.error('Ошибка регистрации:', error)
+        await bot.sendMessage(chatId, `Произошла ошибка при регистрации: ${error.message}`)
     }
+}
     if (msg.text === '/login') {
         await bot.sendMessage(msg.chat.id, 'Выберите роль', {
             reply_markup: {
