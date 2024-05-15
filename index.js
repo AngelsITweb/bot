338896bca5e3d1a650312e7df8e22e30a5e22be6
AngelsITweb <147707018+AngelsITweb@users.noreply.github.com>
2024-05-15 15:44:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
+const to
+
+// ЗАМЕНИТЬ НА ТОКЕН БОТА
 const token = '7066498966:AAGe0PJTKhty_1S-8gy7016myjsIfYztT0Q'
 
 const TelegramBot = require('node-telegram-bot-api')
 const axios = require("axios");
 
+// ЗАМЕНИТЬ ЭТУ УРЛУ НА УРЛУ БЭКА
+const api_url = 'https://my-garage.site'
 const bot = new TelegramBot(token, { polling: true })
 
+async function checkUserExists(msg) {
+    const res = await axios.post('https://my-garage.site/api/users/register', {
+        telegramId: msg.from.id,
+        username: msg.from.username,
+        nickname: msg.from.first_name,
+    }, {
+        headers: {
+            'registration-request': 'true',
+        },
+    });
+    if (typeof res.data === 'string' && res.data.includes('уже')) {
+        return true
+    }
+}
 
-const backendDomain = process.env.DOMAIN
 
 bot.on('message', async (msg) => {
     console.log('started')
     if (msg.text === '/start') {
-    const chatId = msg.chat.id
-    const telegramId = msg.from.id.toString()
-    const username = msg.from.username || 'не указан'
-    const nickname = msg.from.first_name || 'не указан'
-    
-    await bot.sendMessage(chatId, `Отправляю запрос на регистрацию.\nТелеграм ID: ${telegramId}\nUsername: ${username}\nNickname: ${nickname}`)
-    
-    try {
-        const res = await axios.post('https://my-garage.site/api/users/register', { telegramId: telegramId, username: username, nickname: nickname }, {
-            headers: {
-                'telegram-id': telegramId
-            }
-        })
-        console.log(res.statusText)
-        await bot.sendMessage(chatId, `Регистрация прошла успешно ${res.data.username}`)
-    } catch (error) {
-        console.error('Ошибка регистрации:', error)
-        await bot.sendMessage(chatId, `Произошла ошибка при регистрации: ${error.message}`)
+        const chatId = msg.chat.id
+        await bot.sendMessage(chatId, `Рады вас видеть, ${msg.from.first_name}! Проводится регистрация, пожалуйста подождите`)
+        const userExists = await checkUserExists(msg);
+        if (userExists) {
+            await bot.sendMessage(chatId, 'Вы уже зарегистрированы!');
+        } else {
+            const res = await axios.post('http://localhost:3000/api/users/register', {
+                telegramId: msg.from.id,
+                username: msg.from.username,
+                nickname: msg.from.first_name,
+            }, {
+                headers: {
+                    'registration-request': 'true',
+                },
+            });
+            await bot.sendMessage( chatId, 'Вы успешно зарегистрировались!');
+        }
     }
-}
     if (msg.text === '/login') {
         await bot.sendMessage(msg.chat.id, 'Выберите роль', {
             reply_markup: {
@@ -38,7 +54,7 @@ bot.on('message', async (msg) => {
                     [{
                         text: 'Продавец',
                         web_app: {
-                            url: 'https://mygarage-eight.vercel.app/seller-panel'
+                            url: 'https://mygaragewebapp-ajgrj870z-siqqdevs-projects-2a9a6541.vercel.app/seller-panel'
                         }
                     }
                     ],
@@ -46,7 +62,7 @@ bot.on('message', async (msg) => {
             }
         })
     }
-    if (msg.text === '/manager') {
+    if (msg.text === '/managerGSDvsdvafsf') {
         await bot.sendMessage(msg.chat.id, 'Выберите роль', {
             reply_markup: {
                 inline_keyboard: [
@@ -54,7 +70,7 @@ bot.on('message', async (msg) => {
                         {
                             text: 'Менеджер',
                             web_app: {
-                                url: 'https://mygarage-eight.vercel.app/manager-panel'
+                                url: 'https://mygaragewebapp-ajgrj870z-siqqdevs-projects-2a9a6541.vercel.app/manager-panel'
                             }
                         }
                     ],
@@ -63,4 +79,4 @@ bot.on('message', async (msg) => {
         })
     }
 })
-
+                            
