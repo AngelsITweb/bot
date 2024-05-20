5d3d71892f3e3d353f5b3a2f02206045fb0c623c
siqq <144137886+siqqdev@users.noreply.github.com>
2024-05-20 14:09:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,91 +1,100 @@
-// ЗАМЕНИТЬ НА ТОКЕН БОТА
-const token = '7066498966:AAGe0PJTKhty_1S-8gy7016myjsIfYztT0Q'
-
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api');
 const axios = require("axios");
 
-// ЗАМЕНИТЬ ЭТУ УРЛУ НА УРЛУ БЭКА
-const api_url = 'https://my-garage.site'
-const bot = new TelegramBot(token, {polling: true})
+const token = '7066498966:AAGe0PJTKhty_1S-8gy7016myjsIfYztT0Q';
+const api_url = 'https://my-garage.site';
+const bot = new TelegramBot(token, {polling: true});
 
 function sleeper(ms) {
-	return function (x) {
-		return new Promise(resolve => setTimeout(() => resolve(x), ms));
-	};
+    return function (x) {
+        return new Promise(resolve => setTimeout(() => resolve(x), ms));
+    };
 }
 
 async function main() {
-	await sleeper(15_000)
-	console.log("Bot is started!!!")
-	bot.on('message', async (msg) => {
-		console.log('started')
-		if (msg.text === '/start') {
-			const chatId = msg.chat.id
-			await bot.sendMessage(chatId, `Рады вас видеть, ${msg.from.first_name}! Проводится регистрация, пожалуйста подождите`)
-			const userExists = await checkUserExists(msg);
-			if (userExists) {
-				await bot.sendMessage(chatId, 'Вы уже зарегистрированы!');
-			} else {
-				const res = await axios.post('https://my-garage.site/api/users/register', {
-					telegramId: msg.from.id,
-					username: msg.from.username,
-					nickname: msg.from.first_name,
-				}, {
-					headers: {
-						'registration-request': 'true',
-						"telegram-id": msg.from.id
-					},
-				});
-				await bot.sendMessage(chatId, 'Вы успешно зарегистрировались!');
-			}
-		}
-		if (msg.text === '/login') {
-			await bot.sendMessage(msg.chat.id, 'Выберите роль', {
-				reply_markup: {
-					inline_keyboard: [
-						[{
-							text: 'Продавец',
-							web_app: {
-								url: 'https://mygarage-webapp.vercel.app/seller-panel'
-							}
-						}
-						],
-					]
-				}
-			})
-		}
-		if (msg.text === '/managerGSDvsdvafsf') {
-			await bot.sendMessage(msg.chat.id, 'Выберите роль', {
-				reply_markup: {
-					inline_keyboard: [
-						[
-							{
-								text: 'Менеджер',
-								web_app: {
-									url: 'https://mygarage-webapp.vercel.app/manager-panel'
-								}
-							}
-						],
-					]
-				}
-			})
-		}
-	})
+    await sleeper(15_000);
+    console.log("Bot is started!!!");
+
+    bot.on('message', async (msg) => {
+        console.log('started');
+        const chatId = msg.chat.id;
+
+        if (msg.text === '/start') {
+            try {
+                await bot.sendMessage(chatId, `Рады вас видеть, ${msg.from.first_name}! Проводится регистрация, пожалуйста подождите`);
+                const userExists = await checkUserExists(msg);
+                if (userExists) {
+                    await bot.sendMessage(chatId, 'Вы уже зарегистрированы!');
+                } else {
+                    const res = await axios.post(`${api_url}/api/users/register`, {
+                        telegramId: msg.from.id,
+                        username: msg.from.username,
+                        nickname: msg.from.first_name,
+                    }, {
+                        headers: {
+                            'registration-request': 'true',
+                            "telegram-id": msg.from.id
+                        },
+                    });
+                    await bot.sendMessage(chatId, 'Вы успешно зарегистрировались!');
+                }
+            } catch (error) {
+                console.error(error);
+                await bot.sendMessage(chatId, 'Произошла ошибка при регистрации. Пожалуйста, попробуйте позже.');
+            }
+        }
+
+        if (msg.text === '/login') {
+            await bot.sendMessage(chatId, 'Выберите роль', {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{
+                            text: 'Продавец',
+                            web_app: {
+                                url: 'https://mygarage-webapp.vercel.app/seller-panel'
+                            }
+                        }]
+                    ]
+                }
+            });
+        }
+
+        if (msg.text === '/managerGSDvsdvafsf') {
+            await bot.sendMessage(chatId, 'Выберите роль', {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{
+                            text: 'Менеджер',
+                            web_app: {
+                                url: 'https://mygarage-webapp.vercel.app/manager-panel'
+                            }
+                        }]
+                    ]
+                }
+            });
+        }
+    });
 }
 
-void main()
+void main();
 
 async function checkUserExists(msg) {
-	const res = await axios.post('https://my-garage.site/api/users/register', {
-		telegramId: msg.from.id,
-		username: msg.from.username,
-		nickname: msg.from.first_name,
-	}, {
-		headers: {
-			'registration-request': 'true',
-		},
-	});
-	if (typeof res.data === 'string' && res.data.includes('уже')) {
-		return true
-	}
+    try {
+        const res = await axios.post(`${api_url}/api/users/register`, {
+            telegramId: msg.from.id,
+            username: msg.from.username,
+            nickname: msg.from.first_name,
+        }, {
+            headers: {
+                'registration-request': 'true',
+            },
+        });
+        if (typeof res.data === 'string' && res.data.includes('уже')) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
 }
